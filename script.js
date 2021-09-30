e72b035d6d044bc7ad17bab1fa91d2065147ebe6
aman meng <amanuelmengestab@gmail.com>
2021-09-30 12:22:11 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+/*const chalk = require('chalk');
+
+
+console.log(chalk.red('Hello world!'));
+*/
+
+
+let name = "Lina, Bob, Fred";
+
+
+let number = 1;
+
+
+let labb = false;
+
+
+let fruit = ["apple", "orange","mango"];
+
+function greet(name) {
+    console.log('Hello ' + name);
+  }
+
+greet('Bob');
+
+let arrayLength = fruit.length;
+for (let i = 0; i < arrayLength; i++){
+    console.log(fruit[i]);
+}
+
+if (labb) {
+    console.log("bob likes mango");
+} else {
+    console.log("bob dont like manago");
+}
